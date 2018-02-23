07a615e9f75becce6123d81d8926b50f55822590
Marc LaFleur <marcla@microsoft.com>
2018-02-23 11:46:33 -0500

    Fixed namespace

--- content/Controllers/HomeController.cs
+++ content/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication2.Controllers
+namespace MiniMvc.Controllers
 {
     public class HomeController : Controller
     {
